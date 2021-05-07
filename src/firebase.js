39320af5489f3254410af82ab45ba836715e34ac
rayvalesh <rayvalesh@gmail.com>
2021-05-07 12:01:38 +0000

    Final commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDxrhLiysYPtYVb2ptdnbb3o79Rk4Rv3S8',
+  authDomain: 'ipldashboard.firebaseapp.com',
+  projectId: 'ipldashboard',
+  storageBucket: 'ipldashboard.appspot.com',
+  messagingSenderId: '459963201574',
+  appId: '1:459963201574:web:3abb77cf9c553bdaa3cee7',
+  measurementId: 'G-05P0TEVVK5',
+}
